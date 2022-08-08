2ba3072769208ff7c956dbd54e701c5662da34f3
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-08-08 10:33:03 -0700

    imports main

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 export class App extends Component {
 	render() {
 		return (
 			<div className="App">
         <Header />
-				<p>Hello!</p>
+				<Main />
 			</div>
 		);
 	}
