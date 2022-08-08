94a33cdbdf02c9fb08f85bef3a1eed5bf9df1f17
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-08-08 10:31:51 -0700

    ass a section component

---
+++ src/components/Section.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Section extends Component {
+  render() {
+    return (
+      <div className='form-container'>Form Div</div>
+    )
+  }
+}
+
+export default Section
