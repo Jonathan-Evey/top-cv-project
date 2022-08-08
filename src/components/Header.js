d390730a1b2e6e8267fa399ac689dca24d832250
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-08-08 09:44:39 -0700

    add a components file, and add a Header component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import "/home/gunnar/Desktop/cv-project/src/styles/header.css"
+
+export class Header extends Component {
+  render() {
+    return (
+      <header>
+          <h1>Resume Generator</h1>
+      </header>
+    )
+  }
+}
+
+export default Header
