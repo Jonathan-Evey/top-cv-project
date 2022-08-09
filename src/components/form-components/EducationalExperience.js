d0fcdbe66523cac2622305999a47d876ff28c7bd
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-08-09 13:49:26 -0700

    update to Input components and added state update functions

--- src/components/form-components/EducationalExperience.js
+++ src/components/form-components/EducationalExperience.js
@@ -1,28 +1,62 @@
 import React, { Component } from 'react'
+import Input from '../Input';
 import "/home/gunnar/Desktop/cv-project/src/styles/education.css"
 
 export class EducationalExperience extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.handleDegreeChange = this.handleDegreeChange.bind(this);
+    this.handleSchoolChange = this.handleSchoolChange.bind(this);
+    this.handleFieldOfStudyChange = this.handleFieldOfStudyChange.bind(this);
+    this.handleDateFromChange = this.handleDateFromChange.bind(this);
+    this.handleDateToChange = this.handleDateToChange.bind(this);
+
+  } 
+
+  handleDegreeChange(value) {
+    this.props.onEducationInfoChange("degree", value);
+  }
+  handleSchoolChange(value) {
+    this.props.onEducationInfoChange("school", value);
+  }
+  handleFieldOfStudyChange(value) {
+    this.props.onEducationInfoChange("fieldOfStudy", value);
+  }
+  handleDateFromChange(value) {
+    this.props.onEducationInfoChange("dateFrom", value);
+  }
+  handleDateToChange(value) {
+    this.props.onEducationInfoChange("dateTo", value);
+  }
+
   render() {
+    const degree = this.props.educationData.degree;
+		const school = this.props.educationData.school;
+		const fieldOfStudy = this.props.educationData.fieldOfStudy;
+		const dateFrom = this.props.educationData.dateFrom;
+    const dateTo = this.props.educationData.dateTo;
+
     return (
       <fieldset>
           <legend>Educational Experience</legend>
           
           <label htmlFor="">Highest Educational Experience:</label>
-          <input type="text" name="" id="" placeholder="High School, GED, Bachelor's Degree, ect"/>
+          <Input type="text" name="" inputId="" inputPlaceholder="High School, GED, Bachelor's Degree, ect" value={degree} onInputChange={this.handleDegreeChange}/>
 
           <label htmlFor="">School Name:</label>
-          <input type="text" name="" id="" placeholder="Example University of Washington" />
+          <Input type="text" name="" inputId="" inputPlaceholder="Example University of Washington" value={school} onInputChange={this.handleSchoolChange}/>
 
           <label htmlFor="">Title of Study:</label>
-          <input type="text" name="" id="" placeholder="Computer Science" />
+          <Input type="text" name="" inputId="" inputPlaceholder="Computer Science" value={fieldOfStudy} onInputChange={this.handleFieldOfStudyChange}/>
 
           <fieldset className='study-container'>
             <legend>Dates of Study</legend>
             <label htmlFor="">From:</label>
-            <input type="date" name="" id="" /> 
+            <Input type="date" name="" inputId="" value={dateFrom} onInputChange={this.handleDateFromChange}/> 
 
             <label htmlFor="">To:</label>
-            <input type="date" name="" id="" /> 
+            <Input type="date" name="" inputId="" value={dateTo} onInputChange={this.handleDateToChange}/> 
             
           </fieldset>
           <button>Save</button>
